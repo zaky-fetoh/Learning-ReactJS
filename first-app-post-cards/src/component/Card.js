c8b16d8f9f28f951136ca6f606232ce9ba77b39e
Zaky fetoh <zaky.fetoh@gmail.com>
2023-02-12 01:56:06 +0200

    .

--- first-app-post-cards/src/component/Card.js
+++ first-app-post-cards/src/component/Card.js
@@ -3,9 +3,9 @@ import "./Card.css"
 function Card(props){
     console.log(props.children)
     return(
-        <dev className="container">
+        <h3 className="container">
             {props.children}
-        </dev>
+        </h3>
     )
 }
 
