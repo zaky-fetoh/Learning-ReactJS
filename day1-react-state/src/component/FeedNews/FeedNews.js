2f3f2986d20b88b9519f01f38bd17bdcd9c203fb
Zaky fetoh <zaky.fetoh@gmail.com>
2023-02-16 00:50:39 +0200

    style Module is added to FeedNews

--- day1-react-state/src/component/FeedNews/FeedNews.js
+++ day1-react-state/src/component/FeedNews/FeedNews.js
@@ -1,7 +1,7 @@
-import "./FeedNews.css"
+import styles from "./FeedNews.module.css"
 
 function Wall(props){
-    return (<div className="wall">
+    return (<div className={styles.wall}>
         {props.children}
     </div>);
 }
