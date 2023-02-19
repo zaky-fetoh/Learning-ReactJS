186e19bc545db043d7c9349ff5680f117c6d1ce9
Zaky fetoh <zaky.fetoh@gmail.com>
2023-02-19 23:28:40 +0200

    log context is added

---
+++ day4-loggin-page/src/store/login-context.js
@@ -0,0 +1,22 @@
+import react from "react" 
+
+
+export const LogginContext = react.useContext({
+    isLoggin: false, 
+    doLoggin: ()=>{}, 
+    doLoggout: ()=>{}, 
+})
+
+
+export function LogginContextProv(props){
+    const [loggin, setloggin] = react.useState(false);
+    const doLogginFn = ()=>{setloggin(true)};
+    const doLoggoutFn= ()=>{setloggin(false)};
+    return <LogginContext.Provider value={{
+        isLoggin: loggin, 
+        doLoggin: doLogginFn, 
+        doLoggout: doLoggoutFn, 
+    }}>{props.children}
+    </LogginContext.Provider>
+}
+
